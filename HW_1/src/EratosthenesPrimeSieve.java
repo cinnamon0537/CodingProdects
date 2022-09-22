b92db09c6322d3f188c768f7d85b0a56be261023
cinnamon0537 <benjamin.feichtlbauer@gmail.com>
2022-09-22 10:12:36 +0200

    2. Commit

---
+++ HW_1/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,14 @@
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    @Override
+    public boolean isPrime(int p) {
+        boolean[] array = new boolean[p];
+
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
---
+++ HW_1/src/PrimeSieve.java
@@ -0,0 +1,6 @@
+public interface PrimeSieve {
+    // Ueberprueft ob p eine Primzahl ist oder nicht.
+    public boolean isPrime (int p) ;
+    // Gibt alle Primzahlen biszur Obergrenze auf der Konsole aus.
+    public void printPrimes( ) ;
+}
