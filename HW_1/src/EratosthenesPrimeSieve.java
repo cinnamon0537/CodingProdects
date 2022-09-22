f8367cb2d1b341a02f462726fb8f2b4b9bba077e
cinnamon0537 <benjamin.feichtlbauer@gmail.com>
2022-09-22 10:33:24 +0200

    3. Commit

--- HW_1/src/EratosthenesPrimeSieve.java
+++ HW_1/src/EratosthenesPrimeSieve.java
@@ -1,14 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
+
+
+    private ArrayList<Boolean> array = new ArrayList<Boolean>();
+
+    public EratosthenesPrimeSieve(int upperLimit) {
+        for(int i = 0; i < upperLimit; i++){
+            array.set(i, true);
+        }
+
+        for(int i = 2; i < upperLimit; i++){
+
+            if(array.get(i) == true){
+
+                for(int j = 1; i < upperLimit; i *= 2){
+
+                    array.set(j, false);
+                }
+            }
+        }
+    }
 
     @Override
     public boolean isPrime(int p) {
-        boolean[] array = new boolean[p];
-
-        return false;
+        return array.get(p);
     }
 
     @Override
     public void printPrimes() {
+        boolean[] array = new boolean[];
 
+
+
+
+        return false;
     }
 }
