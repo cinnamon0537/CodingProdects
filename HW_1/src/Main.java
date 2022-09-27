3b33c0100a50d939b021b3d129e924ee4730b2d2
cinnamon0537 <benjamin.feichtlbauer@gmail.com>
2022-09-27 08:54:26 +0200

    Final Commit

--- HW_1/src/Main.java
+++ HW_1/src/Main.java
@@ -1,16 +1,2 @@
-import java.util.Scanner;
-
-
-public class Main {
-    public static Scanner scanner = new Scanner(System.in, "Windows-1258");
-
-    public static void main(String strg) {
-        System.out.println("Geben Sie die Obergrenze ein:");
-
-        int upperLimit = scanner.nextInt();
-
-        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(upperLimit);
-
-        eps.printPrimes();
-    }
+package PACKAGE_NAME;public class Main {
 }
